890e5b55e07e9fcc078bb35a3f2987bab216c50f
junhan <junhan@g2.nctu.edu.tw>
2017-10-15 19:41:28 +0800

    change the file location

--- ns_hw1.py
+++ ns_hw1.py
@@ -3,24 +3,7 @@ from fractions import gcd
 import sys
 import base64
 
-
 pub = []
-
-'''
-def egcd(a, b):
-    if a == 0:
-        return (b, 0, 1)
-    else:
-        g, y, x = egcd(b % a, a)
-        return (g, x - (b // a) * y, y)
-
-def modinv(a, m):
-    g, x, y = egcd(a, m)
-    if g != 1:
-        raise Exception('modular inverse does not exist')
-    else:
-        return x % m
-'''
 
 def egcd(a, b):
 	x,y, u,v = 0,1, 1,0
@@ -32,7 +15,7 @@ def egcd(a, b):
 	return gcd, x, y
         
 for i in range(0,13):
-    pub.append ( RSA.importKey(open('public{}.pub'.format(i) ).read()))
+    pub.append ( RSA.importKey(open('./publicKeys/public{}.pub'.format(i) ).read()))
     
 for i in range(1,13):
     for j in range(i+1,13):
@@ -50,10 +33,10 @@ for i in range(1,13):
             
             private_key1 = RSA.construct((pub[i].n,pub[i].e,d1))
             private_key2 = RSA.construct((pub[j].n,pub[j].e,d2))
-            f = open('private' + str(i) + '.pem','wb')
+            f = open('./publicKeys/private' + str(i) + '.pem','wb')
             f.write(private_key1.exportKey('PEM'))
             f.close()
-            f = open('private' + str(j) + '.pem','wb')
+            f = open('./publicKeys/private' + str(j) + '.pem','wb')
             f.write(private_key2.exportKey('PEM'))
             f.close()
             
